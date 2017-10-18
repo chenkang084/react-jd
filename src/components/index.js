a40dc58d8839329650ac5f2e512ac1f3d8121b1a
Kang Kang1 Chen <chenkang1@Lenovo.com>
2017-10-18 17:01:29 +0800

    add menus

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import GreySearchBar from "./SearchBars/Grey.SearchBar";
+import RedSearchBar from "./SearchBars/Red.SearchBar";
+import JdCarousel from "./Carousel/Carousel";
+import Menus from "./Menus/Menus";
+
+export { GreySearchBar, RedSearchBar, JdCarousel, Menus };
