f264e1523f9e963c683b88fc7dcf17be9cb9f1df
Kang Kang1 Chen <chenkang1@Lenovo.com>
2017-10-25 16:46:47 +0800

    update antd-mobile2

--- src/components/NavTabs/NavTabs.js
+++ src/components/NavTabs/NavTabs.js
@@ -10,29 +10,31 @@ export class NavTabs extends React.Component {
 
   render() {
     return (
-      <TabBar
-        unselectedTintColor="#181818"
-        tintColor="#f13030"
-        barTintColor="white"
-        hidden={this.props.hidden}
-      >
-        {this.props.menus &&
-          this.props.menus.map((item, index) => {
-            return (
-              <TabBar.Item
-                icon={<div style={{ ...item.icon }} />}
-                selectedIcon={<div style={{ ...item.selectedIcon }} />}
-                title={item.title}
-                key={index}
-                selected={item.selected === item.title}
-                badge={item.badge}
-                onPress={item.onPress}
-              >
-                {item.render && item.render()}
-              </TabBar.Item>
-            );
-          })}
-      </TabBar>
+      <div style={{ position: "fixed", height: "100%", width: "100%", top: 0 }}>
+        <TabBar
+          unselectedTintColor="#181818"
+          tintColor="#f13030"
+          barTintColor="white"
+          hidden={this.props.hidden}
+        >
+          {this.props.menus &&
+            this.props.menus.map((item, index) => {
+              return (
+                <TabBar.Item
+                  icon={<div style={{ ...item.icon }} />}
+                  selectedIcon={<div style={{ ...item.selectedIcon }} />}
+                  title={item.title}
+                  key={index}
+                  selected={item.selected === item.title}
+                  badge={item.badge}
+                  onPress={item.onPress}
+                >
+                  {item.render && item.render()}
+                </TabBar.Item>
+              );
+            })}
+        </TabBar>
+      </div>
     );
   }
 }
